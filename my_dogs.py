9596abc90ed5ec8d1aeb2bd235b454f4cc6467c1
louiedweck <dweck.louis@gmail.com>
2019-11-20 18:32:16 -0800

    classes

---
+++ my_dogs.py
@@ -0,0 +1,10 @@
+from dog import Dog
+
+my_dog = Dog("Rex", "SuperDog")
+my_other_dog = Dog("Annie", "Superdog")
+# print(my_dog)
+print(my_dog.name)
+print(my_dog.breed)
+my_dog.bark()
+print(my_other_dog.name)
+my_other_dog.bark()
